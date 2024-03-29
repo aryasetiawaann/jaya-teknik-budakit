df46e2bf6e3c7c245d01dacd98f3bae342b70f20
Syam Chaidayatullah <syamsamsungj36@gmail.com>
2024-03-29 15:10:21 +0700

    add portfolio admin & phone number

---
+++ app/Policies/PhoneNumberPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\PhoneNumber;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+
+class PhoneNumberPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, PhoneNumber $phoneNumber): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if (Auth::check()) {
+            return false;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, PhoneNumber $phoneNumber): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, PhoneNumber $phoneNumber): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, PhoneNumber $phoneNumber): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, PhoneNumber $phoneNumber): bool
+    {
+        if (Auth::check()) {
+            return true;
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+}
