e10920ab0713d73a2b1bbfe064f3dbe4ce2ad53c
Syam Chaidayatullah <syamsamsungj36@gmail.com>
2024-03-30 10:20:22 +0700

    fix font home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,6 @@
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <body>
@@ -65,7 +56,7 @@
     </section>
 
     <section class="porto" id="benefit">
-        <h1>Lorem ipsum dolor sit.</h1>
+        <h3>Lorem ipsum dolor sit.</h3>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere in nostrum, aspernatur est asperiores vel.
         </p>
         <div class="porto-list">
