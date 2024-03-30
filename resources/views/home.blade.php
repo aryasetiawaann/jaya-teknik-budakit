a3796269f9e2ec1d7952a4340f98701316b2d955
Syam Chaidayatullah <syamsamsungj36@gmail.com>
2024-03-30 10:10:29 +0700

    fix aos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,23 +9,21 @@
     <link rel="stylesheet" href="/css/home.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/navbar.css">
-    <link rel="stylesheet" href="/css/aos.css">
-    <script src="/js/aos.js" defer>
-        < script src = "/app.js"
-        defer >
-    </script>
+    <script src="/app.js" defer></script>
 
     <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
 
-    {{-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> --}}
-    {{-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> --}}
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap')
-    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <body>
@@ -40,8 +38,9 @@
                     Dunia Pompa Air Bersama Kami</p>
                 <div class="hero-button" data-aos="fade-up" data-aos-duration="1000">
                     <a href="https://tokopedia.link/16I4HNAImIb" target="_blank">
-                        <button class="hero-order-btn">Order Sekarang <svg width="29" height="25"
-                                viewBox="0 0 29 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <button class="hero-order-btn">Order Sekarang <svg
+                                width="29" height="25" viewBox="0 0 29 25" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <g clip-path="url(#clip0_0_38)">
                                     <path
                                         d="M15.2215 15.2302L14.6818 15.6868C14.6818 15.6868 13.3972 16.7713 9.89223 13.81C6.38722 10.8486 7.67179 9.76409 7.67179 9.76409L8.01103 9.47582C8.84962 8.76824 8.92909 7.63128 8.19725 6.80073L6.70273 5.10436C5.79652 4.07626 4.04698 3.94018 3.00912 4.8171L1.1469 6.38949C0.633304 6.82492 0.289327 7.38736 0.330841 8.01228C0.437593 9.61189 1.28923 13.052 6.03849 17.0656C11.076 21.3212 15.8027 21.4905 17.7349 21.3373C18.347 21.2889 18.8783 21.0249 19.3065 20.662L20.9908 19.2388C22.1295 18.2782 21.8093 16.6302 20.3527 15.9579L18.0872 14.9107C17.1312 14.4692 15.9688 14.5992 15.2215 15.2302Z"
@@ -129,7 +128,8 @@
                         echo 'active';
                     } else {
                         echo '';
-                    } ?>" href="/category/{{ $item->id }}#produk">
+                    } ?>"
+                        href="/category/{{ $item->id }}#produk">
                         <li data-id="{{ $item->id }}">{{ $item->name }}
                         </li>
                     </a>
